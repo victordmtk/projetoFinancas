06aa15350242f38c5d16a018ae7bf4a27d95f103
Victor Demetriuk <victor.dmtk@gmail.com>
2022-07-19 08:08:00 -0300

    correçoes

--- selects.sql
+++ selects.sql
@@ -1,10 +1,44 @@
 -- QUANTIDADE DE ASSINATURAS DE CADA PLANO
-SELECT planoid, COUNT(assinaturaid) FROM assinaturas GROUP BY planoid 
+SELECT
+    planoid,
+    COUNT(assinaturaid)
+FROM
+    assinaturas
+GROUP BY
+    planoid 
+
+
 -- VALOR TOTAL RECEBIDO COM AS ASSINATURAS
-SELECT SUM(preco) AS TOTAL  FROM PLANOS 
-LEFT JOIN Assinaturas ON Assinaturas.planoId = planos.planoId
+SELECT
+    SUM(preco) AS TOTAL
+FROM
+    PLANOS
+    LEFT JOIN Assinaturas ON Assinaturas.planoId = planos.planoId 
+	
+	
 --VALOR POR ASSINATURA
-SELECT planos.planoId, SUM(preco) AS [valor por plano]  FROM planos 
-LEFT JOIN Assinaturas ON Assinaturas.planoId = planos.planoId
-GROUP BY planos.planoId
-ORDER BY [valor por plano]
+SELECT
+    planos.planoId,
+    SUM(preco) AS [valor por plano]
+FROM
+    planos
+    LEFT JOIN Assinaturas ON Assinaturas.planoId = planos.planoId
+GROUP BY
+    planos.planoId
+ORDER BY
+    [valor por plano] 
+	
+
+--select pra ver a média dos lancamentos
+SELECT
+    AVG(valor)
+FROM
+    RegistrosOperacoes AS [média dos lançamentos]
+
+
+--select pra ver o maior valor já lançado por um cliente
+SELECT
+    MAX(valor)
+FROM
+    RegistrosOperacoes AS [maior valor registrado]
+
